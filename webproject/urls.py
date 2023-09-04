55a3804142ce8bee6c0b357de02a58deba56b275
PawelOchal <opawel262@gmail.com>
2023-09-04 19:34:28 +0200

    rebuilding the project from sratch

--- webproject/urls.py
+++ webproject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for myproject project.
+URL configuration for webproject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -16,9 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('projects/', views.projects, name='projects'),
 ]
