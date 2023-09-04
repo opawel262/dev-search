dd184f31ecb005f99c591c4f995b7495f8bbdfcc
PawelOchal <opawel262@gmail.com>
2023-09-04 18:07:16 +0200

    created and configured new app 'projects'

--- myproject/settings.py
+++ myproject/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Add your apps here to enable them
+    'projects.apps.ProjectsConfig',
 ]
 
 MIDDLEWARE = [
