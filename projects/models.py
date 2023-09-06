8c92ae3dc02f54953f0725d3c4000371d10db1fd
PawelOchal <opawel262@gmail.com>
2023-09-06 12:52:19 +0200

    added new model for tag and added new fields in  Project model - (tags,all_votes, votes_ratio\ )

--- projects/models.py
+++ projects/models.py
@@ -2,19 +2,31 @@ from django.db import models
 import uuid
 # Create your models here.
 
+
+class Tag(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False, unique=True)
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f"TAG NAME - '{self.name}'"
+
+
 class Project(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False, unique=True)
     title = models.CharField(max_length=50)
     description = models.TextField(null=True, blank=True)
     demo_link = models.URLField(null=True, blank=True)
     source_code = models.URLField(null=True, blank=True)
+    all_votes = models.IntegerField(default=0, null=True, blank=True)
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True)
+    tags = models.ManyToManyField(Tag, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return f"PROJECT TITLE - '{self.title}'"
     
  
-class Review(models.model):
+class Review(models.Model):
     VOTE_TYPE = (
         ('up', 'Up Vote'),
         ('down', 'Down Vote')
